8d6a7074f97071d80049bf326cbe9348041d1dd7
alateeqi <jadm20066@gmail.com>
2023-08-28 20:03:38 +0300

    Update main.py
    class work 3

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 # write your code here
+favorite_animals =["dog","cat","monkey","rabbit"]
+favorite_animals[2]
+print(f"the second element in the list is{favorite_animals[2]}")
+favorite_animals.remove("monkey")
+print(favorite_animals)
+favorite_animals.append("deer")
+for oneanimal in favorite_animals:
+    print(f"i love {oneanimal}")
+    number=[1,2,3,4,5]
+    number_sum=(0)
+for sum in number:
+    number_sum += sum
+print(number_sum)
